e2407c4196cec6e700c6cc0b666bc8ea46f691fb
Daniel Lehr <daniel@qipp.com>
2016-12-25 12:57:11 +0100

    Add `build:prod` with Babili.

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,24 @@
+const BabiliPlugin = require('babili-webpack-plugin')
+
+let plugins = []
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(new BabiliPlugin())
+}
+
 module.exports = {
   entry: './src/App.jsx',
   output: {
     path: 'static/js',
     filename: 'bundle.js'
   },
+  plugins: plugins,
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel',
+        loader: 'babel-loader',
         query: {
           presets: ['es2015', 'react'],
           plugins: ['transform-runtime']
